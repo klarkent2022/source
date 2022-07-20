c2028998afd2a0afac344d68f5ae3ecc3e057144
whyknot2003 <issagulov1001@gmail.com>
2022-07-21 03:03:31 +0900

    local

--- backend_node/Backend/Config/index.js
+++ backend_node/Backend/Config/index.js
@@ -1,11 +1,11 @@
 let config = {
-    port:80,
-    server_url: 'http://localhost:80',
+    port: 3000,
+    server_url: 'http://10.0.2.2:3000',
     db: {
         "host": "localhost",
         "user": "root",
         "password": "",
-        "database": "another_class1",
+        "database": "pettime1",
         "connectionLimit": 30,
         "multipleStatements": true
     }
